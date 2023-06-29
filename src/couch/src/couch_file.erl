75af0c1547d3eed4ef6df466cb97b1c426a6513e
Russell Branca <chewbranca@apache.org>
2023-06-29 16:11:40 -0700

    Fix reply

--- src/couch/src/couch_file.erl
+++ src/couch/src/couch_file.erl
@@ -541,7 +541,7 @@ handle_call({pread_iolist, Pos}, _From, File) ->
             {ok, IoList, Checksum};
         <<0:1/integer, Len:31/integer>> ->
             {Iolist, _} = read_raw_iolist_int(File, NextPos, Len),
-            {reply, {ok, Iolist, <<>>}, File}
+            {ok, Iolist, <<>>}
     end,
     maybe_cache(File#file.tab, {Pos, Resp}),
     {reply, Resp, File};
